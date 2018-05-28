6c260e4c0a53da3a64d4739d0bc8384660573998
Nikolaj Keist <keistnikolaj@gmail.com>
2018-05-28 12:52:07 +0200

    docs
    added documentation in ViewModel

--- classes/viewmodel.class.php
+++ classes/viewmodel.class.php
@@ -13,13 +13,25 @@ abstract class ViewModel
 		$this->template = $template;
 	}
 
+	/**
+     * The Template to render
+     * @return string (template name)
+	 */
 	public function getTemplate()
 	{
 		return $this->template;
 	}
 
+	/**
+	 * Returns the rendering model based on the Template #getTemplate
+	 * @return array
+	 */
 	abstract function getModel();
 
+	/**
+	 * Function that is called before rendering, intended for calculations, database queries etc
+	 * @param $params Array that are given in the route e.g /user/{userId} => $params['userId']
+	 */
 	abstract function run($params);
 }
 ?>
